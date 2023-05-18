1be3dd2cef1a252cd8415b10a8ee6e07714b79b8
Nicolas Duval-Hall <nduvalhall@gmail.com>
2023-05-18 23:26:35 +0100

    add file watcher to neo-tree to auto update on changes

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,10 +2,8 @@ require("neo-tree").setup {
     close_if_last_window = true,
     filesystem = {
         follow_current_file = true,
+        use_libuv_file_watcher = true,
     },
-    buffers = {
-        follow_current_file = true,
-    }
 }
 
 vim.keymap.set("n", "<leader>e", "<cmd>NeoTreeFocus<CR>")
