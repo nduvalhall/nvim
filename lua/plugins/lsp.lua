5e8e0deda0477f50ecfd64858f34da77ca6d7d1f
Miles Duval-Hall <miles.duval-hall@in-space.co.uk>
2023-05-21 11:06:40 +0100

    formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,25 +1,25 @@
-require('mason').setup()
-require('mason-lspconfig').setup({
+require("mason").setup()
+require("mason-lspconfig").setup({
     ensure_installed = {
-        'lua_ls',
-        'tsserver',
-        'pyright',
-        'rust_analyzer',
-        'marksman',
-        'clangd',
-        'julials',
+        "lua_ls",
+        "tsserver",
+        "pyright",
+        "rust_analyzer",
+        "marksman",
+        "clangd",
+        "julials",
     }
 })
-require('mason-null-ls').setup({
+require("mason-null-ls").setup({
     ensure_installed = {
-        'prettier',
-        'black',
+        "prettier",
+        "black",
     }
 })
 
 -- format on save
 vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
-local null_ls = require('null-ls')
+local null_ls = require("null-ls")
 local sources = {
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.black,
@@ -28,58 +28,58 @@ null_ls.setup({
     sources = sources,
 })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local on_attach = function(_, _)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-    vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, {})
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-    vim.keymap.set('n', 'gl', vim.diagnostic.open_float, {})
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+    vim.keymap.set("n", "go", vim.lsp.buf.type_definition, {})
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+    vim.keymap.set("n", "gl", vim.diagnostic.open_float, {})
 end
 
 
-require('lspconfig').lua_ls.setup {
+require("lspconfig").lua_ls.setup {
     on_attach = on_attach,
     capabilites = capabilities,
     settings = {
         Lua = {
             diagnostics = {
                 globals = {
-                    'vim'
+                    "vim"
                 },
             },
         },
     },
 }
 
-require('lspconfig').tsserver.setup {
+require("lspconfig").tsserver.setup {
     on_attach = on_attach,
     capabilites = capabilities,
 }
 
-require('lspconfig').pyright.setup {
+require("lspconfig").pyright.setup {
     on_attach = on_attach,
     capabilites = capabilities,
 }
 
-require('lspconfig').rust_analyzer.setup {
+require("lspconfig").rust_analyzer.setup {
     on_attach = on_attach,
     capabilites = capabilities,
 }
 
-require('lspconfig').marksman.setup {
+require("lspconfig").marksman.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
 
-require('lspconfig').clangd.setup {
+require("lspconfig").clangd.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
 
-require('lspconfig').taplo.setup {
+require("lspconfig").taplo.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
