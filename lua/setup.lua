7b986cc120985d1eea88409466faa82f056170f1
Miles Duval-Hall <miles.duval-hall@in-space.co.uk>
2024-01-12 20:22:12 +0000

    replaced peek with markdown-preview

--- lua/setup.lua
+++ lua/setup.lua
@@ -19,16 +19,13 @@ local plugins = {
     { "folke/tokyonight.nvim", name = "tokyonight", priority = 1000 },
 
     {
-        "toppair/peek.nvim",
-        event = { "VeryLazy" },
-        build = "deno task --quiet build:fast",
-        config = function()
-            require("peek").setup({
-                app = 'firefox'
-            })
-            vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-            vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
         end,
+        ft = { "markdown" },
     },
 
     "nanozuki/tabby.nvim",
