a3e929744bfef6ab68ca6ba01f67ec4dd2154b89
Miles Duval-Hall <nduvalhall@gmail.com>
2024-05-08 19:45:38 +0100

    fixed colorscheme

--- lua/nduvalhall/colorscheme.lua
+++ lua/nduvalhall/colorscheme.lua
@@ -1,11 +1,3 @@
-require("tokyonight").setup({
-	transparent = true,
-	styles = {
-		sidebars = "transparent",
-		floats = "transparent",
-	},
-})
-
 vim.cmd("colorscheme tokyonight")
 vim.cmd("hi StatusLine guibg=None")
 vim.cmd("hi StatusLineNC guibg=None")
