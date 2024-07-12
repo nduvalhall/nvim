48a6ad57e6a3468a643bf7b802d691abf90cb19b
nduvalhall <nduvalhall@gmail.com>
2024-07-12 13:56:55 +0100

    updated oil

--- lua/nduvalhall/plugins/oil.lua
+++ lua/nduvalhall/plugins/oil.lua
@@ -3,8 +3,12 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
 			keymaps = {
 				["<Esc>"] = "actions.close",
+				["<leader>e"] = "actions.close",
 			},
 		})
 		vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>")
