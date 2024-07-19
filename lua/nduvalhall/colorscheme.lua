240bff0bcfd0f850ea1df6516135d04def2fe4b5
Miles Duval-Hall <nduvalhall@gmail.com>
2024-07-19 06:09:51 +0100

    update

--- lua/nduvalhall/colorscheme.lua
+++ lua/nduvalhall/colorscheme.lua
@@ -1,3 +1,2 @@
+vim.o.background = "dark"
 vim.cmd("colorscheme tokyonight-moon")
-
-vim.opt.relativenumber = true
--- lua/nduvalhall/plugins/init.lua
+++ lua/nduvalhall/plugins/init.lua
@@ -5,6 +5,7 @@ return {
 	require("nduvalhall.plugins.gruvbox"),
 	require("nduvalhall.plugins.tokyonight"),
 	require("nduvalhall.plugins.nightfox"),
+	require("nduvalhall.plugins.gruvbox"),
 	require("nduvalhall.plugins.comment"),
 	require("nduvalhall.plugins.textcase"),
 	require("nduvalhall.plugins.oil"),
