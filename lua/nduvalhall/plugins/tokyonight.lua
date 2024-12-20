63af4167a5bdf21b9cf333a6b8ca65aed6af73d9
nduvalhall <nduvalhall@gmail.com>
2024-12-20 15:14:16 +0000

    A

--- lua/nduvalhall/plugins/tokyonight.lua
+++ lua/nduvalhall/plugins/tokyonight.lua
@@ -1,6 +1,19 @@
 return {
   'folke/tokyonight.nvim',
+  lazy = false,
+  priority = 1000,
+  opts = {},
   config = function()
+    require('tokyonight').setup({
+      styles = {
+        comments = {
+          italic = false,
+        },
+        keywords = {
+          italic = false,
+        },
+      },
+    })
     vim.cmd('colorscheme tokyonight-moon')
   end,
 }
--- lua/nduvalhall/plugins/zen-mode.lua
+++ lua/nduvalhall/plugins/zen-mode.lua
@@ -1,5 +1,6 @@
 return {
   'folke/zen-mode.nvim',
+  dependencies = { 'folke/twilight.nvim', opts = {} },
   config = function()
     vim.keymap.set('n', '<leader>z', '<Cmd>ZenMode<CR>')
   end,
