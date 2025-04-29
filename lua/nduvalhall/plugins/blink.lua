c004ecf3f0dfbed48de86450dc085678f8e14c33
nduvalhall <nduvalhall@gmail.com>
2025-04-29 23:01:57 +0100

    Updated blink to hide by default

--- lua/nduvalhall/plugins/blink.lua
+++ lua/nduvalhall/plugins/blink.lua
@@ -4,11 +4,12 @@ return {
   version = 'v0.*',
 
   opts = {
-    keymap = { preset = 'default' },
+    keymap = { preset = 'default', ['<C-e>'] = { 'hide', 'show', 'fallback' } },
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = 'mono',
     },
-    signature = { enabled = true },
+    signature = { enabled = true, trigger = { enabled = false } },
+    completion = { menu = { auto_show = false } },
   },
 }
