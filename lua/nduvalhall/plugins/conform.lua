f7c05a70d0e85aac2d3c96cfd81d6a688c6b4ef9
nduvalhall <nduvalhall@gmail.com>
2025-05-01 09:25:46 +0100

    added conform

---
+++ lua/nduvalhall/plugins/conform.lua
@@ -0,0 +1,28 @@
+return {
+    'stevearc/conform.nvim',
+    opts = {},
+    config = function()
+        require('conform').setup({
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = 'fallback',
+            },
+            formatters_by_ft = {
+                lua = { 'stylua' },
+                python = { 'ruff' },
+                javascript = { 'prettier' },
+                javascriptreact = { 'prettier' },
+                typescript = { 'prettier' },
+                typescriptreact = { 'prettier' },
+                css = { 'prettier' },
+                scss = { 'prettier' },
+                html = { 'prettier' },
+                vue = { 'prettier' },
+                json = { 'prettier' },
+                jsonc = { 'prettier' },
+                markdown = { 'prettier' },
+                sh = { 'shfmt' },
+            },
+        })
+    end,
+}
